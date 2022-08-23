57c1fe8af6d43fa0979aa7fa9d317cb4c8e2dcb6
Dev2023 <e2194608@cmaisonneuve.qc.ca>
2022-08-23 15:15:00 -0400

    Configuration du template ateliers terminé

--- template-ateliers.php
+++ template-ateliers.php
@@ -18,20 +18,24 @@
             $type_de_formation = get_field("type_de_formation");
             $date_de_debut = get_field("date_de_debut");
             $duree = get_field("duree");
-            $date_de_debut = get_field("date_de_debut");
-            $date_de_debut = get_field("date_de_debut");
+            $heure = get_field("heure");
+            $nombre_dheures_par_seance = get_field("nombre_dheures_par_seance");
+            $niveau_de_dificultee = get_field("niveau_de_dificultee");
+            $heure = get_field("heure");
             $prix = get_field("prix");
             echo "<p class='animateur'>Animateur : " . $animateur . "</p>";
             echo "<p class='type_de_formation'>type_de_formation : " . $type_de_formation . "</p>";
-            echo "<p class='animateur'>Animateur : " . $animateur . "</p>";
-            echo "<p class='produit__prix'>Prix : " . $date . "</p>";
-            /* echo "<p class='produit__heure'>L'heure : " . $heure . "</p>"; */
+            echo "<p class='date_de_debut'>Date de début : " . $date_de_debut . "</p>";
+            echo "<p class='duree'>Durée : " . $duree . "</p>";
+            echo "<p class='heure'>L'heure : " . $heure . "</p>";
             echo "<p class='produit__description'>Description : " . $description . "</p>";
-            
-            $image = get_field('image');
+            echo "<p class='niveau_de_dificultee'>Niveau de dificultée : " . $niveau_de_dificultee . "</p>";
+            echo "<p class='lieu'>Lieu : " . $lieu . "</p>";
+            echo "<p class='produit__prix'>Prix : " . $prix . "</p>";
+            /* $image = get_field('image');
                 if( !empty( $image ) ): ?>
                     <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                <?php endif; ?>   
+                <?php endif; ?>    */
             
         </article>
     <?php endif; ?>
